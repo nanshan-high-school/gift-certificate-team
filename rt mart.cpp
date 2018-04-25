7ba70a214047681c26453b0e494274fa726539d1
Hansen Chien <32165157+hansen033@users.noreply.github.com>
2018-04-25 22:47:22 +0800

    Update rt mart.cpp

--- rt mart.cpp
+++ rt mart.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 //product商品數量、p陣價格、total有幾種排列組合 、rank陣排列方法 
 int main(){
-	cout << "你要在RT-mart買幾樣產品啊??(9樣以內)";
+	cout << "你要在RT-mart買幾樣產品啊??";
 	int product;
 	cin >> product;
 	int p[product];
@@ -10,32 +10,33 @@ int main(){
 		cout << "請輸入第" << t + 1 << "產品的金額:";
 		cin >> p[t]; 
 	}
-	int total = 1;
+	int totalR = 1;
 	for(int t = 1, p = 2 ; t < product ; t ++){
-		total += p;
+		totalR += p;
 		p = p*2;
 	}
-	int rank;
+	int rank[totalR][product/2];
+	for(int t1 = 0 ; t1 < totalR ; t1 ++){
+		for(int t2 = 0 ; t2 < product / 2 ; t2 ++){
+			rank[t1][t2] = 0;
+		}
+	}
+	for(int t1 = 0 ; t1 < totalR ; t1 ++){
+		int m = t1 + 1;
+		for(int t2 = 0 ; t2 < product / 2 ; t2 ++){
+			rank[t1][t2] = m;
+			for(t3 = 0 ; t3 < 5 ; t3 ++){
+				if(rank[t1][t2] > product){
+					rank[t1][t2] % product + 
+					if(rank[t1][t2] > product){
+					
+					}
+				}
+			} 
+		}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	}
+
 	
 	
 	
