aa3d6cf4f7c0af4fa6895eeee3c980b7fb675b4e
Hansen Chien <32165157+hansen033@users.noreply.github.com>
2018-04-27 23:40:50 +0800

    Update rt mart.cpp
    完成!!!!

--- rt mart.cpp
+++ rt mart.cpp
@@ -7,19 +7,54 @@ int main(){
 	cin >> product;
 	int p[product];
 	for(int t = 0 ; t < product ; t++){
-		cout << "The NO." << t + 1 << "product price?";
+		cout << "What is the NO." << t + 1 << " product price?";
 		cin >> p[t]; 
 	}
-	int rankT = prw(2,product)
-	
-	
-	
-
-	
-	
-	
-	
-	
+	int rankT = pow(2,product);
+	int rank[rankT][product];
+	for(int x = 0 ; x < product ; x ++){
+		int change = pow(2, x);
+		int add = 0;
+		for(int y = 0 ; y < rankT ; y ++){
+			rank[y][x] = add;
+			if(y % change == 0){
+				add = (add == 0) ? 1 : 0;
+			}
+		}
+	}
+	int A[rankT] = {0}, B[rankT] = {0};
+	for(int t = 0 ; t < rankT ; t ++){
+		for(int t2 = 0 ; t2 < product ; t2 ++){
+			if(rank[t][t2] == 1){
+				A[t] += p[t2];
+			} else{
+				B[t] += p[t2];
+			}
+		}
+	}
+	int refound[rankT];
+	for(int t = 0 ; t < rankT ; t ++){
+		refound[t] = A[t] % 100 + B[t] % 100;
+	}
+	int most = 0, mostR = 0;
+	for(int t = 0 ; t < rankT ; t ++){
+		if(refound[t] > mostR){
+			most = t;
+		}
+	}
+	cout << "A should buy ";
+	for(int t = 0 ; t < product ; t ++){
+	 	if (rank[most][t] == 1){
+	 		cout << "No." << t + 1 << " ";
+		}
+	}
+	cout << "product(s).\nB should buy ";
+	for(int t = 0 ; t < product ; t ++){
+	 	if (rank[most][t] == 0){
+	 		cout << "No." << t + 1 << " ";
+		}
+	}
+	cout << "product(s).";
 	system("Pause");
 	return 0;
 }
